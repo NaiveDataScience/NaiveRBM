f64ae1d758ea8658851b65d9be40bac0d84cb577
hazelnutsgz <guozhen.she@intel.com>
2018-12-19 12:09:29 +0800

    commit the right code

--- rbm.py
+++ rbm.py
@@ -1,8 +1,7 @@
-# python: 2.7
-# encoding: utf-8
-
 import numpy as np
 import math
+import matplotlib.pyplot as plt
+
 
 def new_map(func, tensor):
     if type(tensor) != np.ndarray:
@@ -24,24 +23,32 @@ def new_compare(func, tensor1, tensor2):
 
     return np.array(ret)
 
+def sigmoid(x):
+    try:
+        ret = 1.0 / (1.0 + math.exp(-x))
+        return ret
+    except:
+        return 0
+
 class RBM:
     """Restricted Boltzmann Machine."""
 
-    def __init__(self, n_hidden=2, n_observe=784):
+    def __init__(self, n_hidden=100, n_observe=784, alpha=0.01):
         """Initialize model."""
-        self.v = np.random.rand(n_observe, 1) # hidden variables [n_observe, 1]
-        self.h = np.random.rand(n_hidden, 1) # hidden variables [n_hidden, 1]
-        self.W = np.random.rand(n_observe, n_hidden) # Weight matrix
-        self.a = np.random.rand(n_observe, 1) # bias for the visual variables
-        self.b = np.random.rand(n_hidden, 1) # bias for the hidden variables
+        self.n_observe = n_observe
+        self.n_hidden = n_hidden
+        self.v = np.random.normal(0, 1, [n_observe, 1]) # visual variables [n_observe, 1]
+        self.h = np.random.normal(0, 1, [n_hidden, 1]) # hidden variables [n_hidden, 1]
+        self.W = np.random.normal(0, 1, [n_observe, n_hidden]) # Weight matrix
+        self.a = np.zeros([n_observe, 1]) # bias for the visual variables
+        self.b = np.zeros([n_hidden, 1]) # bias for the hidden variables
         # 请补全此处代码
 
-        self.alpha = 0.01 # learning rate
-        pass
+        self.alpha = alpha # learning rate
+        # pass
 
 
-
-    def train(self, data, max_epoch=10, max_process=5):
+    def train(self, data, max_epoch=3):
         """Train model using data."""
 
         # 请补全此处代码
@@ -51,28 +58,35 @@ class RBM:
             for v in data:
                 ## CD loss
                 v = v.reshape(-1, 1)
-                h_dist = self.sigmoid(
-                    np.matmul(np.transpose(self.weight), v) + self.b)
-                h_sample = self._sample_binary(h_dist)
+                h_dist = new_map(sigmoid, \
+                    np.matmul(np.transpose(self.W), v) + self.b)
 
-                v_dist = self.sigmoid(
-                    np.matmul(np.transpose(self.weight), v) + self.a)
-                v_sample = self._sample_binary(v_dist)
+                # h_sample = self._sample_binary(h_dist)
+                h_sample = h_dist
+                v_dist = new_map(sigmoid, \
+                    np.matmul(self.W, h_sample) + self.a)
 
-                h_dist2 = self.sigmoid(
-                    np.matmul(np.transpose(self.weight), v) + self.b)
-                h_sample2 = self._sample_binary(h_dist2)
+                # v_sample = self._sample_binary(v_dist)
+                v_sample = v_dist
+                h_dist2 = new_map(sigmoid, \
+                    np.matmul(np.transpose(self.W), v_sample) + self.b)
+                
+                # h_sample2 = self._sample_binary(h_dist2)
+                h_sample2 = h_dist2
                 ## Update weight
-                self.weight += self.alpha * \
-                    (np.matmul(v_n, h_sample.transpose()) -
-                        np.matmul(v_sample, h_sample2.transpose()))
+                self.W += self.alpha * \
+                    (np.matmul(v, np.transpose(h_sample)) -
+                        np.matmul(v_sample, np.transpose(h_sample2)))
 
                 self.a += self.alpha * (v - v_sample)
                 self.b += self.alpha * (h_sample - h_sample2)
 
+            print (self._energy(v, h_sample2))
 
-    def sigmod(x):
-        return 1 / (1 + math.exp(-x))
+            np.save("w.npy", W)
+            np.save("a.npy", a)
+            np.save("b.npy", b)
+    
 
     def _sample_binary(self, p):
         """
@@ -87,39 +101,49 @@ class RBM:
         return new_compare(compare_func, \
                     new_map(get_random, p), p)
 
+    def _energy(self, visible, hidden):
+        return -np.inner(self.a.flatten(), visible.flatten()) - np.inner(self.b.flatten(), hidden.flatten()) \
+            -np.matmul(np.matmul(np.transpose(visible), self.W), hidden)
 
-    def sample(self, iter_times=10):
+
+
+    def sample(self, new_v, iter_times=3):
         """Sample from trained model."""
         # 请补全此处代码
-        new_v = np.random(self.n_observe, 1)
+
+        self.W = np.load("w.npy")
+        self.a = np.load("a.npy").reshape(-1, 1)
+        self.b = np.load("b.npy").reshape(-1, 1)
         for iter in range(iter_times):
-            p_h = self.sigmod(np.matmul( \
-                    self.W.traponse(), new_v) + self.b)
-
-            new_h = self._sample_binary(p_h)
-            p_v = self.sigmod(np.matmul( \
-                    self.W, new_h) + self.a)
-            new_v = self._sample_binary(p_v)
-
+            p_h = new_map(sigmoid, np.matmul(np.transpose(self.W), new_v) + self.b)
+            
+            # new_h = self._sample_binary(p_h)
+            new_h = p_h
+            p_v = new_map(sigmoid, np.matmul(self.W, new_h) + self.a)
+            # new_v = self._sample_binary(p_v)
+            new_v = p_v
+            # import pdb;pdb.set_trace()
         return new_v
 
 # train restricted boltzmann machine using mnist dataset
 if __name__ == '__main__':
     # load mnist dataset, no label
-    mnist = np.load('mnist_bin.npz')  # 60000x28x28
-    import pdb;pdb.set_trace()
-    n_imgs, n_rows, n_cols = mnist.shape
-    img_size = n_rows * n_cols
-    print (mnist.shape)
+    mnist = np.load('mnist_bin.npy')  # 60000x784
+    n_imgs, img_size = mnist.shape
 
     # construct rbm model
-    rbm = RBM(2, img_size)
+    rbm = RBM(100, img_size)
 
     # train rbm model using mnist
-    rbm.train(mnist)
+    # rbm.train(mnist[0:6000], 3)
 
     # sample from rbm model
-    s = rbm.sample()
-    import pdb;pdb.set_trace()
+    random_index = np.random.randint(len(mnist))
+    raw_img = mnist[random_index].reshape(-1, 1)
+    gen_img = rbm.sample(raw_img, 3)
 
-
+    plt.subplot(1,2,1)
+    plt.imshow(raw_img.reshape((28, 28)), cmap="gray")
+    plt.subplot(1,2,2)
+    plt.imshow(gen_img.reshape((28, 28)), cmap="gray")
+    plt.savefig("result.png")
--- test_map.py
+++ test_map.py
@@ -1,27 +1,94 @@
+# python: 3.7
+# encoding: utf-8
+
 import numpy as np
-import math
+import matplotlib.pyplot as plt
 
+def sigmoid(x):
+    return 1.0 / (1 + np.exp(-x))
 
+class RBM:
+    """Restricted Boltzmann Machine."""
+    
+    def __init__(self, n_hidden=500, n_observe=784, learning_rate = 0.01):
+        """Initialize model."""
+        
+        # 请补全此处代码
+        self.a = np.random.normal(0, 1, [n_observe,1])
+        self.b = np.random.normal(0, 1, [n_hidden, 1])
+        self.W = np.random.normal(0, 1, size=(n_observe, n_hidden))
+        self.v_0 = np.zeros([n_observe, 1])
+        self.h_0 = np.zeros([n_hidden, 1])
+        self.v_1 = np.zeros([n_observe, 1])
+        self.h_1 = np.zeros([n_hidden, 1])
+        self.learning_rate = learning_rate
+    
+    def train(self, data, time_steps):
+        """Train model using data."""
+        data = np.reshape(data, (data.shape[0], data.shape[1]))
+        
+        # 请补全此处代码
+        for epoch in range(time_steps):
+            N = data.shape[0]
+            for i in range(N):
+                self.v_0 = data[i].reshape(28 * 28, -1)
+                # import pdb;pdb.set_trace()
+                self.h_0 = sigmoid(self.b + np.dot(np.transpose(self.W), self.v_0))
 
-def add1(x):
-	return x + 1
+                self.v_1 = sigmoid(self.a + np.dot(self.W, self.h_0))
+                self.h_1 = sigmoid(self.b + np.dot(np.transpose(self.W), self.v_1))
 
-def _sample_binary(p):
-    """
-    Sample the binary vector by given probability tensor
-    """
-    def get_random(s):
-        return np.random.uniform(0, 1)
+                self.W = self.W + self.learning_rate * (np.dot(self.v_0, np.transpose(self.h_0))
+                                              - np.dot(self.v_1, np.transpose(self.h_1)))
 
-    def compare_func(a, b):
-        return 0 if a < b else 1
+                self.a = self.a + self.learning_rate * (self.v_0 - self.v_1)
+                self.b = self.b + self.learning_rate * (self.h_0 - self.h_1)
+                if (i+1) % 1000 == 0:
+                    print("epoch:%s, step:%s" % (epoch, i+1))
+        np.save('w.npy', self.W)
+        np.save('a.npy', self.a)
+        np.save('b.npy', self.b)
 
-    mapping = new_map(get_random, p)
-    return new_compare(compare_func, \
-                mapping, p)
+    def sample(self):
+        """Sample from trained model."""
+        
+        # 请补全此处代码
+        mnist = np.load('mnist_bin.npy')
 
+        index = np.random.randint(mnist.shape[0])
+        im = mnist[index].reshape(28*28, -1)
+        v = mnist[index].reshape(28*28, -1)
+    
+        h = sigmoid(self.b + np.dot(np.transpose(self.W), v))
+        for i in range(5):
+            v = sigmoid(self.a + np.dot(self.W, h))
+            h = sigmoid(self.b + np.dot(np.transpose(self.W), v))
+        return im, v
 
+# train restricted boltzmann machine using mnist dataset
 if __name__ == '__main__':
-	s = _sample_binary(np.array([[0.3,0.4],[0.5, 0.8]]))
-	import pdb;pdb.set_trace()
+
+    # load mnist dataset, no label
+    mnist = np.load('mnist_bin.npy').reshape(-1, 28*28)  # 60000x784
+    
+    n_imgs, img_size = mnist.shape
+    print(mnist.shape)
+
+    # construct rbm model
+    rbm = RBM(500, img_size)
+
+    time_steps = 3
+    # train rbm model using mnist
+    rbm.train(mnist, time_steps)
+    
+    # sample from rbm model
+    im, new_im = rbm.sample()
+    im = np.reshape(im, (28, 28))
+    new_im = np.reshape(new_im, (28, 28))
+    plt.subplot(1, 2, 1)
+    plt.imshow(im.astype("float"), cmap='gray')
+    plt.subplot(1, 2, 2)
+    plt.imshow(new_im.astype("float"), cmap='gray')
+    plt.show()
+
 
